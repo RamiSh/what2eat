6a0b269ad4a3dc535b89aa76aef8d68739adbb5f
Rami Shareef <rami.shareef@gmail.com>
2018-12-06 08:00:15 -0500

    Added nodejs and expressjs as backend

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require("body-parser");
+
+const app = express();
+const status = require('./server/routes/status');
+
+// inject custom middleware into the pipeline
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, 'dist/what2eat')));
+app.use('/api', status);
+
+// redirect all requests (except api) to the index page.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/what2eat/index.html'));
+});
+
+const appPort = process.env.PORT || 4600;
+app.listen(appPort, () => console.log(`listening on port ${appPort}`));
